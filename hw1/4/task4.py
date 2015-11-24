89c3f5668e9963ec9214c60e30fec9112bbdcc8b
Roenke <roenke54@gmail.com>
2015-11-24 19:26:15 +0300

    add skip list implementation

--- hw1/4/task4.py
+++ hw1/4/task4.py
@@ -8,7 +8,8 @@ class Node(object):
         levels = 1
         while random.random() >= 0.5:
             levels += 1
-        self.levels = [None] * levels
+        self.height = levels
+        self.next = [None] * levels
         self.value = value
 
 
@@ -16,12 +17,6 @@ class SkipList(object):
     def __init__(self):
         self._head = Node(None)
         self.__size = 0
-
-    def _find_last_less(self, item, level):
-        node = self._head
-        while node.levels[level] and node.levels[level].elem < item:
-            node = node.levels[level]
-        return node
 
     def insert(self, item):
         """
@@ -31,20 +26,34 @@ class SkipList(object):
         >>> sl.insert(1)
         False
         """
-        update = self._path_to_less(item)
-        if self.__size > 0 and update[0].next[0] is not None and \
-                update[0].next[0].elem == item:
+        if self.contains(item):
             return False
-        height = self._random_height()
-        new = self._SkipNode(item, height)
-        min_height = min(len(update), height)
-        for level in range(min_height):
-            new.next[level] = update[level].next[level]
-            update[level].next[level] = new
-        self.head.next += [new] * (height - self.head.height())
+
+        current_node = self._head
+        new_node = Node(item)
+        current_level = min(self._head.height - 1, new_node.height - 1)
+        if new_node.height > self._head.height:
+            new_level_count = new_node.height - self._head.height
+            self._head.height += new_level_count
+            self._head.next.extend([new_node] * new_level_count)
+
+        while current_level >= 0:
+            next_node = current_node.next[current_level]
+            if next_node is None:
+                if new_node.height > current_level:
+                    current_node.next[current_level] = new_node
+                current_level -= 1
+                continue
+            if next_node.value > item:
+                # Move down
+                new_node.next[current_level] = next_node
+                current_node.next[current_level] = new_node
+                current_level -= 1
+            else:
+                # Move right
+                current_node = next_node
         self.__size += 1
         return True
-
 
     def remove(self, item):
         """
@@ -54,7 +63,30 @@ class SkipList(object):
         >>> sl.remove(1), sl.remove(1), sl.remove(3)
         (True, False, False)
         """
-        pass
+        if not self.contains(item):
+            return False
+
+        current_level = self._head.height - 1
+        current_node = self._head
+        deleted = None
+        while current_level >= 0:
+            if current_node.next[current_level] is None:
+                current_level -= 1
+                continue
+            if current_node.next[current_level].value < item:
+                current_level -= 1
+                continue
+            if current_node.next[current_level].value > item:
+                current_node = current_node.next[current_level]
+                continue
+            if not deleted:
+                deleted = current_node.next[current_level]
+            current_node.next[current_level] = deleted.next[current_level]
+            current_level -= 1
+
+        self.__size -= 1
+        del deleted
+        return True
 
     def contains(self, item):
         """
@@ -64,7 +96,23 @@ class SkipList(object):
         >>> sl.contains(1), sl.contains(3)
         (True, False)
         """
-        pass
+        levels = len(self._head.next)
+        level = levels - 1
+        current = self._head
+        while level >= 0:
+            if current.next[level] is None:
+                level -= 1
+                continue
+            if current.next[level].value < item:
+                current = current.next[level]
+                continue
+            if current.next[level].value > item:
+                level -= 1
+                continue
+            if current.next[level].value == item:
+                return True
+
+        return False
 
     def size(self):
         """
@@ -80,10 +128,15 @@ class SkipList(object):
         >>> sl.size()
         2
         """
-        pass
+        return self.__size
 
     def __iter__(self):
-        pass
+        def get_next(node):
+            while node:
+                yield node.value
+                node = node.next[0]
+
+        return get_next(self._head.next[0])
 
 
 def make_skip_list(seq):
@@ -94,10 +147,13 @@ def make_skip_list(seq):
     10
     >>> make_skip_list([10] * 10).size()
     1
-    >>> sum(make_skip_list(range(10**6)))
-    499999500000
+    >>> sum(make_skip_list(range(10**5)))
+    4999950000
     """
-    pass
+    skip_list = SkipList()
+    for s in sorted(seq, reverse=True):
+        skip_list.insert(s)
+    return skip_list
 
 
 def make_list(skip_list):
@@ -106,12 +162,13 @@ def make_list(skip_list):
     []
     >>> make_list(make_skip_list(range(3, 0, -1)))
     [1, 2, 3]
-    >>> sum(make_list(make_skip_list(range(10**6))))
-    499999500000
+    >>> sum(make_list(make_skip_list(range(10**5))))
+    4999950000
     """
     return list(skip_list)
 
 
 if __name__ == '__main__':
     import doctest
+
     doctest.testmod()
