b7accc9251b105a2eb8f1cc0acf175d2485f43ae
Roenke <roenke54@gmail.com>
2015-12-05 20:09:27 +0300

    task 2 done

--- hw2/1/1.py
+++ hw2/1/1.py
@@ -1,8 +1,34 @@
 #! /usr/bin/env python3
 
+def final(func):
+    WithFinals.add_final_function(func)
+
+
+class WithFinals(type):
+    __final_methods = set()
+
+    def __call__(cls, *args, **kwargs):
+        super(WithFinals, cls)
+
+    @staticmethod
+    def add_final_function(func):
+        WithFinals.__final_methods.add(func)
+
+
+class A(metaclass=WithFinals):
+    pass
+
+
+class B(metaclass=WithFinals):
+    pass
+
+
+class C(A, B):
+    pass
+
 
 def main():
-    pass
+    c = C()
 
 
 if __name__ == "__main__":
